c364353c89539b300ef15c1b00eabfcdaff1573f
Ernie Val <erndval@gmail.com>
2019-03-18 14:31:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,14 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-  programmer_hash = :yukihiro_matsumoto
+
+ programmer_hash = 
+ {
+  :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }
+  
 
 	programmer_hash = 
  		{
@@ -83,9 +90,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = "LISP"
 
 programmer_hash
     
