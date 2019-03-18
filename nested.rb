33abd278be0a0fc72886f7ddb891bdcb048dd261
Ernie Val <erndval@gmail.com>
2019-03-18 14:15:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,7 +69,6 @@ programmer_hash[:yukihiro_matsumoto]:known_for]["Ruby"]
 programmer_hash[:yukihiro_matsumoto]:languages]["LISP"]
 programmer_hash[:yukihiro_matsumoto]:languages]["C"]
     
-}
 
 	programmer_hash = 
  		{
