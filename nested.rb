17a1665cda11e726526c593ddee02d34531a0d3f
Ernie Val <erndval@gmail.com>
2019-03-18 14:33:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,16 +65,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
- programmer_hash = 
- {
-  :yukihiro_matsumoto => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-  }
- }
-  
-
+ 
 	programmer_hash = 
  		{
  		
@@ -92,7 +83,12 @@ def adding_matz
         }
      }
 
-programmer_hash
+programmer_hash = {
+  :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+ }
     
 end
 
